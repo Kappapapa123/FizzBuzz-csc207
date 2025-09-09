c49bd17d466661ca347d15c2783423e576089262
Kappapapa123 <suzuki.keishi.227@gmail.com>
2025-09-09 18:13:02 -0400

    update multiples function

--- src/lab1/Multiples.java
+++ src/lab1/Multiples.java
@@ -2,21 +2,17 @@ package lab1;
 
 public class Multiples {
     public static int multiples(int n, int a, int b) {
-        int x = ((n - 1) / a);
-        int y = ((n - 1) / b);
-        int z = ((n - 1) / (a * b));
-        return x + y - z;
-    }
-
-    private static int multiples() {
-        int x = (999 / 3);
-        int y = (999 / 5);
-        int z = (999 / (3 * 5));
-        return x + y - z;
+        int count = 0;
+        for (int i=1; i<n; i++) {
+            if (i % a == 0 || i % b == 0){
+                count++;
+            }
+        }
+        return count;
     }
 
     public static void main(String[] args) {
-        int count = multiples(1000, 3, 5);
+        int count = multiples(1000,4,4);
         System.out.println(count);
     }
 }
